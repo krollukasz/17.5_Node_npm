6c37ed32ace00dc87e5bb2d94f229d46c87f8225
Łukasz Król <lionel@o2.pl>
2019-01-13 16:13:53 +0100

    Add color module, change text color in getOSinfo function

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require("os"); // zaimportowanie modułu "os"
+var colors = require("colors"); // zaimportowanie modułu "colors"
 var time = require("../modules/conversionTime"); // zaimportowanie modułu conversionTime z folderu "modules"
 
 function getOSinfo () {
@@ -12,12 +13,12 @@ function getOSinfo () {
   var cpu = os.cpus()[0].model;
   var uptime = os.uptime();
   var userInfo = os.userInfo();
-  console.log("System: ", type);
-  console.log("Release: ", release);
-  console.log("CPU Model: ", cpu);
-  console.log("Uptime: ", time.print(uptime));
-  console.log("User info: ", userInfo.username);
-  console.log("Home dir: ", userInfo.homedir);
+  console.log("System: ".green, type);
+  console.log("Release: ".yellow, release);
+  console.log("CPU Model: ".grey, cpu);
+  console.log("Uptime: ".blue, time.print(uptime));
+  console.log("User info: ".green, userInfo.username);
+  console.log("Home dir: ".red, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
