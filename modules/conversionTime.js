034f40c51a11382fc7255d96ec0351d9b21c1c48
Łukasz Król <lionel@o2.pl>
2019-01-24 21:28:13 +0100

    Add function parameter, modify condition in function

--- modules/conversionTime.js
+++ modules/conversionTime.js
@@ -1,7 +1,7 @@
 var os = require("os"); // zaimportowanie modułu "os"
 var colors = require("colors"); // zaimportowanie modułu "colors"
 
-function conversionTime() { // funkcja konwertująca czas
+function conversionTime(uptime) { // funkcja konwertująca czas
   var uptime = os.uptime(); // zapisane w zmiennej wartości "uptime" pobranej z modułu "os"
 
   var hours = parseInt(uptime / 3600); // zapisanie w zmiennej ilości obliczonych godzin; wartość "uptime" / 3600 (sekund) daje ilość godzin
@@ -9,14 +9,14 @@ function conversionTime() { // funkcja konwertująca czas
   var minutes = parseInt(remainingSeconds / 60); // zamipasnie w zmennej ilości mimut (pozostałe sekundy / 60 sekund)
   var seconds = parseInt(remainingSeconds % 60) // zapisanie w zmiennej ilości sekund pozostałych sekund
 
-  if (minutes < 0) {
-    console.log("Uptime: " + seconds + " sec");
-  } else if (minutes > 0) {
+  if (minutes > 0) {
     if (hours > 0) {
       console.log("Uptime: " + hours + " hrs " + minutes + " min " + seconds + " sec")
     } else {
       console.log("Uptime: " + minutes + " min " + seconds + " sec")
     }
+  } else {
+    console.log("Uptime: " + seconds + " sec")
   }
 }
 
