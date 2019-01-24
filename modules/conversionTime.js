53cdbabc2671b70f34d1495462ed93ef30de532c
Łukasz Król <lionel@o2.pl>
2019-01-24 13:11:03 +0100

    Modify conversionTime function

--- modules/conversionTime.js
+++ modules/conversionTime.js
@@ -1,11 +1,22 @@
-function conversionTime(time) {
-  if (time < 60) { // sprawdzenie, czy wartość jest mniejsza niz 60, wtedy wyświetl sekundy
-    return time + " sec";
-  } else if (time >= 60) { // dla wartości większej lub równej 60 sekund, wyświetl minuty, resztę z dzielenia jako sekundy
-    return (Math.floor(time / 60) + " min " + Math.floor(time % 60) + " sec");
-  } else { // dla wartości większej lub równej 3600 sekund, podziel i wyświetl godziny, resztę podziel przez 60 i wyświel jako minuty, resztę jako sekundy
-    return (Math.floor(time / 3600) + " hour/s" + (Math.floor(time % 3600)/60) + " min " + Math.floor(time % 60) + " sec");
+var os = require("os"); // zaimportowanie modułu "os"
+
+function conversionTime() { // funkcja konwertująca czas
+  var uptime = os.uptime(); // zapisane w zmiennej wartości "uptime" pobranej z modułu "os"
+
+  var hours = parseInt(uptime / 3600); // zapisanie w zmiennej ilości obliczonych godzin; wartość "uptime" / 3600 (sekund) daje ilość godzin
+  var remainingSeconds = parseInt(uptime % 3600); // zapisanie w zmiennej ilości pozostałych sekund po obliczniu godzin ( % - modulo = reszta z dzielenia)
+  var minutes = parseInt(remainingSeconds / 60); // zamipasnie w zmennej ilości mimut (pozostałe sekundy / 60 sekund)
+  var seconds = parseInt(remainingSeconds % 60) // zapisanie w zmiennej ilości sekund pozostałych sekund
+
+  if (minutes < 0) {
+    console.log("Uptime: " + seconds + " sec");
+  } else if (minutes > 0) {
+    if (hours > 0) {
+      console.log("Uptime: " + hours + " hrs " + minutes + " min " + seconds + " sec")
+    } else {
+      console.log("Uptime: " + minutes + " min " + seconds + " sec")
+    }
   }
 }
 
-exports.print = conversionTime; // udostępnenie modułu
+exports.conversionTime = conversionTime; // udostępnenie modułu
