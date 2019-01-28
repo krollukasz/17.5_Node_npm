fb09d9442d8400cc8169ce56e0c8a310da49ba6d
Łukasz Król <lionel@o2.pl>
2019-01-28 17:27:01 +0100

    Modify conversionTime function

--- modules/conversionTime.js
+++ modules/conversionTime.js
@@ -2,21 +2,20 @@ var os = require("os"); // zaimportowanie modułu "os"
 var colors = require("colors"); // zaimportowanie modułu "colors"
 
 function conversionTime(uptime) { // funkcja konwertująca czas
-  var uptime = os.uptime(); // zapisane w zmiennej wartości "uptime" pobranej z modułu "os"
 
   var hours = parseInt(uptime / 3600); // zapisanie w zmiennej ilości obliczonych godzin; wartość "uptime" / 3600 (sekund) daje ilość godzin
   var remainingSeconds = parseInt(uptime % 3600); // zapisanie w zmiennej ilości pozostałych sekund po obliczniu godzin ( % - modulo = reszta z dzielenia)
   var minutes = parseInt(remainingSeconds / 60); // zamipasnie w zmennej ilości mimut (pozostałe sekundy / 60 sekund)
-  var seconds = parseInt(remainingSeconds % 60) // zapisanie w zmiennej ilości sekund pozostałych sekund
+  var seconds = parseInt(remainingSeconds % 60); // zapisanie w zmiennej ilości sekund pozostałych sekund
 
   if (minutes > 0) {
     if (hours > 0) {
-      console.log("Uptime: " + hours + " hrs " + minutes + " min " + seconds + " sec")
+      return (hours + " hrs " + minutes + " min " + seconds + " sec");
     } else {
-      console.log("Uptime: " + minutes + " min " + seconds + " sec")
+      return (minutes + " min " + seconds + " sec");
     }
   } else {
-    console.log("Uptime: " + seconds + " sec")
+    return (seconds + " sec");
   }
 }
 
